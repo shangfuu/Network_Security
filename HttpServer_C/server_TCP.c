b212ef1e6d7d89f5ede91ca85c13d70fe6a6bc2b
shungfu <jordan880805@gmail.com>
2020-04-03 00:50:03 +0800

    Web-browser work

--- HttpServer_C/server_TCP.c
+++ HttpServer_C/server_TCP.c
@@ -6,12 +6,30 @@
 #include <netinet/in.h>
 
 #define PORT 8000
+
+void http_request(int *new_socket){
+	// msg
+	char *msg = "<h1>Hello from server</h1>\n";
+	
+	// Header part
+	char *header = "HTTP/1.1 200 ok\r\n";
+
+	write(*new_socket,header,strlen(header));
+	write(*new_socket,"Content-Type: text/html\r\n",25);
+	
+	char buffer[100];
+	sprintf(buffer,"Content-Length: %d\r\n\r\n",strlen(msg));
+	write(*new_socket,buffer,strlen(buffer));
+	
+	write(*new_socket,msg,strlen(msg));
+}
+
+void http_request();
+
 int main(int argc,char const *argv[]){
 	int server, new_socket;
 	struct sockaddr_in address;
 	int addr_len = sizeof(address);
-
-	char *hello = "Hello from server\n";
 	
 	// Create new socket
 	if((server = socket(AF_INET,SOCK_STREAM,0))==0){
@@ -48,8 +66,9 @@ int main(int argc,char const *argv[]){
 		// Read msg from client
 		read(new_socket,buffer,10000);
 		printf("%s\n",buffer);
-		// Sent mst to client
-		write(new_socket,hello, strlen(hello));
+		// Sent msg to client
+		http_request(&new_socket);
+
 		printf("-----------------Hello MSG sent-----------------\n");
 		close(new_socket);
 	}
