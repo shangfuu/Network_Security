2e1a6d5454d1b8f9cfd52571626fa64baa08e296
shungfu <jordan880805@gmail.com>
2020-04-03 19:49:11 +0800

    Http header with Server, Date,Content-Length,Content-Type

--- HttpServer_C/server_TCP.c
+++ HttpServer_C/server_TCP.c
@@ -4,36 +4,63 @@
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <time.h>
 
 #define PORT 8000
+#define BUFF_SIZE 4096
+
+#define TEST(type,print) printf(type,print);
 
 void http_request(int *new_socket){
 	// msg
 	char *msg = "<h1>Hello from server</h1>\n";
 	
-	// Header part
-	char *header = "HTTP/1.1 200 ok\r\n";
-
+	// Version, Status Code, Status message
+	char *header = "HTTP/1.1 200 ok\r\n";	
 	write(*new_socket,header,strlen(header));
+	// Content-Type
 	write(*new_socket,"Content-Type: text/html\r\n",25);
-	
-	char buffer[100];
-	sprintf(buffer,"Content-Length: %d\r\n\r\n",strlen(msg));
+
+	// Date
+	time_t current = time(NULL);
+	char buffer[BUFF_SIZE];
+	sprintf(buffer,"Date: %s\r",ctime(&current));
 	write(*new_socket,buffer,strlen(buffer));
+	memset(buffer,0,sizeof(buffer));
+	TEST("%s",buffer);
+
+	// Server name
+	char *server_name = "Server: HttpServerC\r\n";
+	write(*new_socket,server_name,strlen(server_name));
+	TEST("%s",server_name);
 	
+	// Content-Length
+	sprintf(buffer,"Content-Length: %ld\r\n",strlen(msg));
+	write(*new_socket,buffer,strlen(buffer));
+	TEST("%s",buffer);
+	
+	write(*new_socket,"\r\n",2);
+	// Send Message
 	write(*new_socket,msg,strlen(msg));
 }
 
 void http_request();
 
 int main(int argc,char const *argv[]){
-	int server, new_socket;
+	int sockFD, new_sockFD;
 	struct sockaddr_in address;
 	int addr_len = sizeof(address);
 	
 	// Create new socket
-	if((server = socket(AF_INET,SOCK_STREAM,0))==0){
+	if((sockFD = socket(AF_INET,SOCK_STREAM,0))==0){
 		perror("Socket Error:");
+		exit(EXIT_FAILURE);
+	}
+
+	// Set Socket to be reusable address
+	int yes = 1;
+	if(setsockopt(sockFD,SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
+		perror("Setting socket option to SO_REUSEADDR");
 		exit(EXIT_FAILURE);
 	}
 
@@ -44,12 +71,12 @@ int main(int argc,char const *argv[]){
 
 	memset(address.sin_zero, '\0',sizeof(address.sin_zero));
 	
-	if(bind(server,(struct sockaddr *)&address,sizeof(address)) < 0){
+	if(bind(sockFD,(struct sockaddr *)&address,sizeof(address)) < 0){
 		perror("Bind Error: ");
 		exit(EXIT_FAILURE);
 	}
 	// Ready to accept new connection
-	if(listen(server,20) < 0){
+	if(listen(sockFD,20) < 0){
 		perror("Listen Error: ");
 		exit(EXIT_FAILURE);
 	}
@@ -57,20 +84,20 @@ int main(int argc,char const *argv[]){
 	while(1){
 		printf("\n----------Waiting fornew connection----------\n");
 		// Takes first connection request and create new socket
-		if((new_socket = accept(server,(struct sockaddr *)&address,(socklen_t*)&addr_len)) < 0){
+		if((new_sockFD = accept(sockFD,(struct sockaddr *)&address,(socklen_t*)&addr_len)) < 0){
 			perror("Accept Error: ");
 			exit(EXIT_FAILURE);
 		}
 
 		char buffer[10000] = {0};
 		// Read msg from client
-		read(new_socket,buffer,10000);
+		read(new_sockFD,buffer,10000);
 		printf("%s\n",buffer);
 		// Sent msg to client
-		http_request(&new_socket);
+		http_request(&new_sockFD);
 
 		printf("-----------------Hello MSG sent-----------------\n");
-		close(new_socket);
+		close(new_sockFD);
 	}
 	return 0;
 }
